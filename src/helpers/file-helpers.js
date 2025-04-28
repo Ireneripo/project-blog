6e7dbf582dea8e1e964aec0851a4997f95a38ec3
Irene Riportella <riportellairene@gmail.com>
2025-04-28 23:34:45 +0200

    feat:✨ Exercise 6: Circular colors widget

--- src/helpers/file-helpers.js
+++ src/helpers/file-helpers.js
@@ -23,8 +23,6 @@ export async function getBlogPostList() {
 }
 
 export const loadBlogPost = React.cache(async function loadBlogPost(slug) {
-  console.log("Reading blog post:", slug);
-
   const rawContent = await readFile(`/content/${slug}.mdx`);
 
   const { data: frontmatter, content } = matter(rawContent);
